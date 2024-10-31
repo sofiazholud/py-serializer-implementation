0fbd50f5f0ccde3e1d3bf87884d0530dbda29ffe
Sofiia Zholud <szholuds@gmail.com>
2024-10-31 12:03:08 +0200

    flake8

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ from car.models import Car
 import json
 from car.serializers import CarSerializer
 
+
 def serialize_car_object(car):
     serializer = CarSerializer(car)
     data = serializer.data
     # Add the id field if it exists on the Car object
-    if hasattr(car, 'id'):
-        data['id'] = car.id
+    if hasattr(car, "id"):
+        data["id"] = car.id
     return json.dumps(data)
+
 
 def deserialize_car_object(json_data):
     data = json.loads(json_data)
